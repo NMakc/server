77bc51f32208c06d478f4b0e48e6273c0678acc5
NMakc <43989132+NMakc@users.noreply.github.com>
2018-10-17 10:25:06 +0300

    2 commit

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){ 
-    res.send('200'); // 
-})
+//***********указываем какой движок используем для шаблонизатора 
+app.set('view engine', 'ejs');
 
+// *************рендер в браузере при запуске сервера
+// app.get('/', function(req, res){ 
+//     res.send('200'); // 
+// })
+// *************рендер в браузере index.html из корневой папки
+// app.get('/', function(req, res){ 
+// res.sendFile(__dirname +'/index.html'); // 
+// })
+//*************рендер в браузере go.ejs из папки views
+app.get('/', function(req, res){ 
+ res.render('go',{newTitle: "ShopTR" }); 
+ })
+
+// запуск сервера на порту 3000
 app.listen(3000, function() {
     console.log('Server started')
 })
